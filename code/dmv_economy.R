ccc4f79ea8a0062331d292663af244318f8a87d1
Emma Cohn <ecohn@epi.org>
2025-07-14 10:22:50 -0400

    changed geo, median earnings

--- code/dmv_economy.R
+++ code/dmv_economy.R
@@ -7,11 +7,12 @@ library(realtalk)
 library(openxlsx2)
 library(here)
 library(ipumsr)
+library(epidatatools)
 
 # Download ACS data
 
 acs_2023_5yr_vars <-c('MET2013', 'PERWT', 'SEX', 'AGE', 'RACE', 'HISPAN', 'CITIZEN', 
-                      'EMPSTAT', 'LABFORCE', 'OCC', 'IND', 'POVERTY', 'INCWAGE')
+                      'EMPSTAT', 'LABFORCE', 'OCC', 'IND', 'POVERTY', 'INCWAGE', 'PWSTATE2')
   
 #acs_samps <- ipumsr::get_sample_info('usa')
 acs_samps <-ipums_data_collections()
@@ -32,7 +33,7 @@ dl_acs2023_5yr <- download_extract(extract = acs2023_5yr,
 # CLean ACS data for employed DMV workers
 
 acs2023_5yr_final <- read_ipums_micro(dl_acs2023_5yr) |>
-  filter(MET2013 == 47900, EMPSTAT == 1) |>
+  filter(PWSTATE2 == 11, EMPSTAT == 1) |>
   janitor::clean_names() |>
   mutate(
     pop=1,
@@ -72,7 +73,7 @@ tipped_workers <- acs2023_5yr_final |>
 # Set up Excel
 tipped_acs <- wb_workbook()
 
-# Number of workers 
+# Number of workers
 
 total_tipped <- tipped_workers |>
   count(tipc == 1, wt = perwt)
@@ -111,13 +112,14 @@ tipped_acs$add_worksheet(sheet = "by_demographics") $
 
 # Poverty rates
 tipped_pov <- tipped_workers |>
-  filter(tipc == 1, poverty != 0) |>  # exclude N/A poverty values
+  filter(poverty != 0) |>  # exclude N/A poverty values
   mutate(below_poverty = if_else(poverty <= 100, 1, 0),
          poverty_200 = if_else(poverty <= 200, 1, 0)) |>
   summarize(total_emp = sum(empstat * perwt, na.rm=TRUE),
             total_pov = sum(below_poverty * perwt, na.rm=TRUE),
             total_pov_200 = sum(poverty_200 * perwt, na.rm = TRUE),
-            n=n()) |>
+            n=n(),
+            .by = tipc) |>
   mutate(pov_share = total_pov/total_emp,
          pov_200_share = total_pov_200/total_emp)
 
@@ -126,9 +128,15 @@ tipped_acs$add_worksheet(sheet = "poverty_rate") $
 
 # Median earnings
 tipped_inc <- tipped_workers |>
-  filter(tipc == 1) |>
-  summarize(
-    earnings = MetricsWeighted::weighted_median(incwage, w=perwt, na.rm=TRUE))
+ # filter(tipc == 1) |>
+  summarise(
+      earnings_median = averaged_median(
+        x = incwage, 
+        w = perwt,  
+        quantiles_n = 9L, 
+        quantiles_w = c(1:4, 5, 4:1)),
+        n=n(),
+        .by = tipc)
 
 tipped_acs$add_worksheet(sheet = "median_earnings") $
   add_data(x = tipped_inc)
